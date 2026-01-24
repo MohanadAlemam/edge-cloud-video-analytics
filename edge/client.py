24be69a023bb85e2a39df838fcee47249e52d7fd
Mohanad Alemam <mohannad.alemam@gmail.com>
2026-01-24 18:45:08 +0000

    feat: complete feed_cloud_function. And start an Orchestration function for edge pipeline to organise and manage edge processes i.e. ‘run_edge_pipeline’.

--- edge/client.py
+++ edge/client.py
@@ -38,7 +38,61 @@ def feed_cloud_jpeg(
                              files=files_payload, timeout=requests_timeout_sec)
     # normalize URL by removing slashes if present and append the /infer endpoint then POST files
 
-    
+    response.raise_for_status()
+    # Error handling for HTTP requests. to avoid failed request or errors (eg 500, 404, 401) being treated as inference
+
+    # parse response body as JSON and return the resulting Python object eg dictionary
+    return response.json()
+
+
+# 2. Orchestrator function to run the edge pipeline
+def run_edge_pipeline(
+        video_path:str,
+        cloud_server_url:str,
+        frame_skip: int = 20,
+        resize_width:int = 640,
+        jpeg_quality:int = 80,
+        frame_change_threshold:float = 20.0,
+        debug_mode:bool = False,
+        debug_frames:int = 5,
+):
+    """"
+    Orchestration function for edge pipeline to organise and mange edge processes.
+
+    - Generate frames using video_frames_generator
+    - Preprocess including resize and grayscale conversion
+    - Decide whether to send. simple change in frame filter
+    - Encode to JPEG bytes and send if interesting
+    - Send the interesting frames to the could serve and receives responses, print results and simple local metrics.
+
+    """
+    # 1. GENERATING FRAMES FROM THE VIDEO
+    print(f"Edge initiating processing pipeline. Server: {cloud_server_url}")
+
+    frames_seen = 0
+    previous_gray = None
+    frames_sent = 0
+
+    for frame_index, timestamp, frame in video_frames_generator(video_path, skip_frames=frame_skip):
+        frames_seen += 1
+
+        print (f"Edge frame {frame_index}, timestamp: {timestamp:.3f} second, (seen {frames_seen})")
+
+
+        # 2. PREPROCESSING FRAMES
+        smaller_frame = resize_frame(frame, target_width=resize_width)
+        # resize the frame
+        gray_and_small = covert_to_grayscale(smaller_frame)
+        # greyscale the frame
+
+
+        # 3. DECIDING IF THE FRAME IS INTERESTING BASED ON HEURISTIC CHANGE
+        significant_change_detected, change_score = interesting_frames(previous_frame = previous_gray,
+                                                                   current_frame = gray_and_small,
+                                                                   difference_threshold = frame_change_threshold)
+
+        print(f"Edge frame change score = {change_score:.2f}.\nIs the frame interesting? {significant_change_detected}")
+        # Call interesting_frames function and assess the change in the frames
 
 
 
@@ -62,21 +116,3 @@ def feed_cloud_jpeg(
 
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-#api_url = "http://localhost:5000/hello"
-api_url = "http://10.0.0.3:5000/hello"
-response = requests.get(api_url)
-rd = response.json()
-pretty = json.dumps(rd, indent=4)
-print(pretty)
