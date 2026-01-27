8bdd72a7f4ce33cc7e2a4ba11a49d950ce816ba3
Mohanad Alemam <mohannad.alemam@gmail.com>
2026-01-27 18:56:47 +0000

    feat: incorporate performance metrics logic and code to monitor and record processes on edge device, network/bandwidth and cloud including: -	total_frames_processed: total frames seen -	total_bytes_sent_to_cloud -	frame_sent_to_cloud: portion of frames send to the cloud -	Frame drop ratio: the ratio of the frames dropped as per the edge logic -	average_rt_ms: average round trip to the could -	slowest_rt_ms: slowest round trip

--- edge/client.py
+++ edge/client.py
@@ -1,21 +1,19 @@
-
 import requests
 # HTTP client, sends data to the cloud
-import cv2
-# for frames/image encoding
 import argparse
 # Commandline argument parsing
-import os
-# for environment variables
 import time
-# Timing utilities and latency measurement
+import numpy as np
+
 
 from video_reader import video_frames_generator
 
 from preprocess import (resize_frame,
-                        covert_to_grayscale,
+                        convert_to_grayscale,
                         encode_to_jpeg_bytes,
-                        interesting_frames)
+                        is_frame_interesting)
+
+from edge_model import EdgeModel #import the edge model to conduct lightweight on-edge processing
 
 
 # 1. a function to feed the could frames
@@ -57,7 +55,7 @@ def run_edge_pipeline(
         debug_frames:int = 5,
 ):
     """"
-    Orchestration function for edge pipeline to organise and manage edge processes.
+    Orchestration function for edge pipeline to organize and manage edge processes.
 
     - Generate frames using video_frames_generator
     - Preprocess including resize and grayscale conversion
@@ -69,68 +67,127 @@ def run_edge_pipeline(
     # 1. GENERATING FRAMES FROM THE VIDEO
     print(f"Edge initiating processing pipeline. Server: {cloud_server_url}")
 
-    frames_seen = 0
-    previous_gray = None
-    frames_sent = 0
+    total_frames_processed = 0
+    previous_grey = None
 
-    for frame_index, timestamp, frame in video_frames_generator(video_path, skip_frames=frame_skip):
-        frames_seen += 1
+    frame_sent_to_cloud = 0
+    cloud_round_trips_ms = []  # track all cloud round trips time ms
+    edge_model_inferences_ms = [] # collects all edge model inference times
+    total_bytes_sent_to_cloud = 0
 
-        print (f"Edge frame {frame_index}, timestamp: {timestamp:.3f} second, (seen {frames_seen})")
+    # 1.1 Create an EdgeModel instance once before the loop.
+    edge_model = EdgeModel()
+
+    for frame_index, timestamp, colored_frame in video_frames_generator(video_path, skip_frames=frame_skip):
+        total_frames_processed += 1
+
+        print (f"Edge frame {frame_index}, timestamp: {timestamp:.3f} second, (seen {total_frames_processed})")
 
 
         # 2. PREPROCESSING FRAMES
-        smaller_frame = resize_frame(frame, target_width=resize_width)
-        # resize the frame
-        gray_and_small = covert_to_grayscale(smaller_frame)
-        # greyscale the frame
+        # resize the frame and  greyscale the frame
+        smaller_frame = resize_frame(colored_frame, target_width=resize_width)
+        grey_and_small = convert_to_grayscale(smaller_frame)
 
+        # Skip change detection for the first one
+        if previous_grey is None:
+            previous_grey = grey_and_small
+            continue # stop and go to the next iteration
 
         # 3. DECIDING IF THE FRAME IS INTERESTING BASED ON HEURISTIC CHANGE
-        significant_change_detected, change_score = interesting_frames(previous_frame = previous_gray,
-                                                                   current_frame = gray_and_small,
+        significant_change_detected, change_score = is_frame_interesting(previous_frame = previous_grey,
+                                                                   current_frame = grey_and_small,
                                                                    difference_threshold = frame_change_threshold)
 
         print(f"Edge frame change score = {change_score:.2f}.\nIs the frame interesting? {significant_change_detected}")
         # Call interesting_frames function and assess the change in the frames
 
+        # Dont send to the cloud if nothing changed
+        if not significant_change_detected:
+            print(f"As per the pixels values no significant change in the video feed was detected."
+                  f"\nNo further processing will be performed.")
+            previous_grey = grey_and_small
+            continue # stop and go to the next iteration
 
-        # 5. SEND ONLY INTERESTING FRAMES TO THE CLOUD FOR INFERENCE
-        if significant_change_detected:
-            # in case the frame is interesting ie significant change occurs
-            # we need to send this frame to the cloud for inference, colored version of the frame as it has more info
+        # 4. LIGHTWEIGHT EDGE MODEL
 
-            jpeg_payload = encode_to_jpeg_bytes(frame=smaller_frame, quality=jpeg_quality)
-            # prepare the colored version of the frame as it has more info
+        # if the fames are significantly different try the edge model (yolo) before sending to the cloud
+        else:
+            send_to_cloud, edge_inference_time_ms = edge_model.yolo_decision(colored_frame)
 
-            send_start = time.time()
+            edge_model_inferences_ms.append(edge_inference_time_ms) # register this edge inference time
+
+            # 5. SEND ONLY INTERESTING FRAMES TO THE CLOUD FOR INFERENCE
+            if send_to_cloud:
+
+                # send the colored frame to the cloud for inference, as it has more info
+
+                jpeg_payload = encode_to_jpeg_bytes(frame=smaller_frame, quality=jpeg_quality)
+                # prepare the colored version of the frame as it has more info
+
+                total_bytes_sent_to_cloud += len(jpeg_payload) # accumulate the total bytes
+
+                send_start = time.time()
             # record the sending time and send the frame
-            try:
-                cloud_response = feed_cloud_jpeg(cloud_server_url = cloud_server_url,
+                try:
+                    cloud_response = feed_cloud_jpeg(cloud_server_url = cloud_server_url,
                                                  jpeg_bytes = jpeg_payload
                                                  )
-                round_trip_time = time.time() - send_start * 1000.0 # measure round trip in ms
-                frames_sent += 1 # up the frames sent counter
+                    round_trip_time = (time.time() - send_start) * 1000.0 # measure round trip in ms
+                    frame_sent_to_cloud += 1 # up the frames sent counter
+                    cloud_round_trips_ms.append(round_trip_time) # collect ms round trip time
 
-                print(f"Edge sent frame{frame_index} .Round trip time: {round_trip_time:.2f} ms."
-                      f"\nServer response: {cloud_response}")
+                    print(f"Edge sent frame{frame_index} .Round trip time: {round_trip_time:.2f} ms."
+                          f"\nServer response: {cloud_response}")
 
-            except Exception as e:
-                print(f"Edge Error related to sending frame: {frame_index}, {e}")
-                # handling errors
+                except Exception as e:
+                    print(f"Edge Error related to sending frame: {frame_index}, {e}")
+                    # handling errors
 
-        previous_gray = gray_and_small
+        previous_grey = grey_and_small
         # current frame becomes previous
 
-        if debug_mode and frames_seen == debug_frames:
+        if debug_mode and total_frames_processed == debug_frames:
             break
 
+    # 6. Metrics
+    frame_drop_ratio = 1 - (frame_sent_to_cloud / total_frames_processed) if total_frames_processed > 0 else 0
+    # a metric to show the usefulness of the edge intelligence/processing and dropping of frames
+    average_round_trip = np.mean(cloud_round_trips_ms) if cloud_round_trips_ms else 0
+    slowest_round_trip = max(cloud_round_trips_ms) if cloud_round_trips_ms else 0
+
+    average_edge_inference_ms = np.mean(edge_model_inferences_ms) if edge_model_inferences_ms else 0
+    total_bytes_sent_kb = total_bytes_sent_to_cloud / 1024 if total_bytes_sent_to_cloud else 0 # convert to kb
+
+    edge_metrics = {
+        "total_frames_processed": total_frames_processed,
+        "frame_drop_ratio": frame_drop_ratio,
+        "avg_edge_inference_time": average_edge_inference_ms,
+    }
+
+
+    network_bandwidth_metrics ={
+        "total_frames_to_cloud": frame_sent_to_cloud,
+        "total_bytes_sent_to_cloud": total_bytes_sent_kb,
+    }
+
+    cloud_metrics = {
+        "avg_rt_ms": average_round_trip,
+        "slowest_rt_ms": slowest_round_trip,
+        "cloud_requests_handled": XXXXXX,
+        "avg_cloud_inference_time": XXX,
+    }
+
     # give a summary at the end
-    print(f"Edge processing completed. Frames seen: {frames_seen}, Frames sent: {frames_sent}")
+    print(f"Edge processing completed:"
+          f"\n\nFrames seen: {total_frames_processed}"
+          f"\n\nFrames sent to the cloud: {frame_sent_to_cloud}"
+          f"\n\nDrop ratio: {frame_drop_ratio}")
+
+    return edge_metrics, network_bandwidth_metrics, cloud_metrics
 
 
-
-# 3. Command-Line Interface (CLI) to run the program from the terminal and control it using text commands and flags
+# 7. Command-Line Interface (CLI) to run the program from the terminal and control it using text commands and flags
 
 if __name__ == "__main__":
     # Runs only when execute python edge/client.py in the terminal.
