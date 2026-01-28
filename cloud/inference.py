58c20e00699d82850fabf450b8cb43bb264dca63
Mohanad Alemam <mohannad.alemam@gmail.com>
2026-01-28 14:26:20 +0000

    feat: complete _load_model fucntion and add _read_frame_request function to Reads frame from a request payload, and decode the bytes into colored frame.

--- cloud/inference.py
+++ cloud/inference.py
@@ -39,6 +39,37 @@ def _load_model(model_name:str):
     if _cloud_model_loaded:
         return _cloud_model
 
+    if not _HAS_YOLO:
+        print("Ultralytics YOLO is not installed in this system.")
+        return None
+    # Try to load the specified model from YOLO
+    try:
+        cloud_model = YOLO(model_name) # load the chosen model
+        cloud_model_loaded = True
+        print(f"YOLO model loaded: {model_name}")
+        return cloud_model
+    except Exception:
+        print(f"Mode {model_name} failed to load from YOLO.")
+        _cloud_model_loaded = False
+        _cloud_model = None
+        return None
+
+def _read_frame_from_request(request):
+    """
+    Reads frame from a request payload, and decode the bytes into colored frame.
+
+    :param request: request payload
+    :return: colored frame, or none
+    """
+    if "image" not in request.files:
+        # JPEG as a multipart file/payload to server/infer produced by .post()
+        return None
+
+    raw_bytes = request.files["image"].read() # read the raw bytes
+    np_arr = np.frombuffer(raw_bytes, dtype=np.uint8) # convert to np array
+    frame = cv2.imdecode(np_arr, cv2.IMREAD_COLOR) # decode to jpeg with RGB channels
+    return frame
+
 
 
 
