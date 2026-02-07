144c83dd433fa0ba1d620bdff5a06ea5388ca950
Mohanad Alemam <mohannad.alemam@gmail.com>
2026-02-07 12:41:06 +0000

    fix: change the end point to infer, instead of inference.

--- edge/cloud_feeder.py
+++ edge/cloud_feeder.py
@@ -14,12 +14,12 @@ def feed_cloud_jpeg(
     it only does the HTTP request and return parsed JSON.
     """
     # prepare multipart or form data payload, field "image" with (filename, bytes, mime-type/ media type eg JPEG image)
-    files_payload = {
+    file_payload = {
         "image": ("frame.jpg", jpeg_bytes, "image/jpeg")
     }
     # will post the in memory JPEG as a multi-part file/payload to server/inference
-    response = requests.post(cloud_server_url.rstrip("/") + "/inference",
-                             files=files_payload, timeout=requests_timeout_sec)
+    response = requests.post(cloud_server_url.rstrip("/") + "/infer",
+                             files=file_payload, timeout=requests_timeout_sec)
     # normalize URL by removing slashes if present and append the /infer endpoint then POST files
 
     response.raise_for_status()
