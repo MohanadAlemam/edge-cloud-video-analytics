77d531e5eec602478ac20a94154aa3ba69b6520b
Mohanad Alemam <mohannad.alemam@gmail.com>
2026-02-07 12:39:03 +0000

    fix : fix the function to name the variable intrusion_metrics instead of intrusion_content.

--- common/metrics_snapshot.py
+++ common/metrics_snapshot.py
@@ -2,31 +2,26 @@ from pathlib import Path
 import json
 
 
-def write_metrics_snapshot(edge_metrics, network_metrics, content_metrics, cloud_metrics= {}):
+def write_metrics_snapshot(edge_metrics, network_metrics, intrusion_metrics, cloud_metrics):
     """
     This function writes the metrics snapshot per frame to a file
 
     :param edge_metrics: edge metrics collected for the current frame
     :param cloud_metrics: cloud metrics collected for the current frame
     :param network_metrics: network metrics collected for the current frame
-    :param content_metrics: content metrics collected for the current frame.
+    :param intrusion_metrics: intrusion metrics collected for the current frame.
     :return: None. write to hard disk
     """
     METRICS_PATH = Path("output/metrics_history.json")
 
-    if not cloud_metrics:
-        snapshot_metrics = {
-            "edge_metrics": edge_metrics,
-            "network_metrics": network_metrics,
-            "content_metrics": content_metrics,
-        }
-    elif isinstance(cloud_metrics, dict):
-        snapshot_metrics = {
-            "edge_metrics": edge_metrics,
-            "network_metrics": network_metrics,
-            "content_metrics": content_metrics,
-            "cloud_metrics": cloud_metrics
-        }
+
+    snapshot_metrics = {
+        "edge_metrics": edge_metrics,
+        "network_metrics": network_metrics,
+        "intrusion_metrics": intrusion_metrics,
+        "cloud_metrics": cloud_metrics
+    }
+
     try:
         if METRICS_PATH.exists():
             raw = METRICS_PATH.read_text().strip()
@@ -51,7 +46,7 @@ def write_metrics_snapshot(edge_metrics, network_metrics, content_metrics, cloud
         temporary.write_text(json.dumps(data, indent=2))
         # write to a temporary file first 'metrics_history.tmp' before replacing the real JSON file
         temporary.replace(METRICS_PATH)
-        print(f"\nMetrics snapshot written to: {METRICS_PATH}")
+        print(f"Metrics snapshot written to: {METRICS_PATH}\n")
 
     except Exception as e:
         print(f"Error: Failed to write the metrics snapshot  {e}")
